41e4c31ccfc988ebfe9eb11139143f540cae121d
unknown <shreekerprabha06@gmail.com>
2026-03-31 14:16:14 +0530

    resolve merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import LandingPage from './pages/LandingPage'
 import { Routes, Route } from 'react-router-dom'
 import DetailsPage from './pages/DetailsPage'
 import MyActiveList from './pages/MyActiveList'
-<<<<<<< HEAD
-import MyCompletedList from './pages/MyCompletedList'
-=======
 import All from './pages/All'
-import Completed from './pages/MyCompletedList'
->>>>>>> 7f85a1402d70434da4ccc80c6f3fdad1f200f436
+import MyCompletedList from './pages/MyCompletedList'
+
+
+
 
 const App = () => {
   return (
@@ -17,12 +16,12 @@ const App = () => {
       <Route path="/" element={<LandingPage/>} />
       <Route path="/:id" element={<DetailsPage/>} />
       <Route path = "/active" element = {<MyActiveList/>}/>
-<<<<<<< HEAD
-      <Route path = "/completed" element = {<MyCompletedList/>}/>
-=======
       <Route path='/all' element = {<All/>}/>
-      <Route path='/completed' element = {<Completed/>}/>
->>>>>>> 7f85a1402d70434da4ccc80c6f3fdad1f200f436
+      <Route path = "/completed" element = {<MyCompletedList/>}/>
+
+      
+   
+
     </Routes>
     </>
   )
