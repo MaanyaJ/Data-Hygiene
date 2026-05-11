bedb3b4ef8b2d5d93f00aa36db0a7592c79e0352
Maanya Jaladi <maanyajaladi@gmail.com>
2026-05-11 15:06:41 +0530

    resolve path for shared package

--- mfe-dashboard/vite.config.js
+++ mfe-dashboard/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { federation } from "@module-federation/vite";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   resolve: {
