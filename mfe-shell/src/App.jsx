fbe87fa8151c7132f1b83c8c17fc020f638fc050
Maanya Jaladi <maanyajaladi@gmail.com>
2026-05-12 11:18:08 +0530

    handle 404

--- mfe-shell/src/App.jsx
+++ mfe-shell/src/App.jsx
@@ -1,8 +1,9 @@
 import React, { Suspense, lazy, useState, useEffect } from 'react'
 import { Routes, Route, useLocation, Navigate, Outlet } from 'react-router-dom'
-import { Box, Snackbar, Alert, Typography } from "@mui/material"
+import { Box, Snackbar, Alert} from "@mui/material"
 import Navbar from './components/Navbar';
 import { Loader } from "@data-hygiene/ui";
+import PageNotFound from './components/PageNotFound';
 
 // Remote MFEs
 const RecordsListPage = lazy(() => import('dashboard/RecordsListPage'));
@@ -63,31 +64,7 @@ const App = () => {
           </Route>
 
           {/* 404 Not Found (No Navbar) */}
-          <Route path="*" element={
-            <Box sx={{ 
-              height: '100vh', 
-              display: 'flex', 
-              flexDirection: 'column', 
-              justifyContent: 'center', 
-              alignItems: 'center',
-              backgroundColor: '#f4f4f4'
-            }}>
-              <Typography variant="h1" fontWeight={900} sx={{ color: '#000', mb: 1 }}>404</Typography>
-              <Typography variant="h5" fontWeight={600} sx={{ mb: 2 }}>Page Not Found</Typography>
-              <Typography color="textSecondary" sx={{ mb: 4 }}>The page you are looking for does not exist.</Typography>
-              <Typography 
-                onClick={() => window.location.href = '/'}
-                sx={{ 
-                  cursor: 'pointer', 
-                  color: '#000', 
-                  textDecoration: 'underline', 
-                  fontWeight: 700 
-                }}
-              >
-                Go back home
-              </Typography>
-            </Box>
-          } />
+          <Route path="*" element= {<PageNotFound/> }/>
         </Routes>
       </Suspense>
 
